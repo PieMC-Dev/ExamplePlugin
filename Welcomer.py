ab2088e7c1c8a467d983ad3882d1fbd986f1726f
skyss0fly <100278495+skyss0fly@users.noreply.github.com>
2023-06-17 10:47:50 +1000

    Create Welcomer.py

---
+++ Welcomer.py
@@ -0,0 +1,13 @@
+import PieMC
+class main():
+  def __init__ player():
+    player.username = username
+    
+    
+  def username(player):
+    PieMC.username = username
+    
+ function welcome():
+  if PieMC.player joined:
+     PieMC.chat(broadcast('Welcome', player.username, 'to', PieMC.servername)
+             
