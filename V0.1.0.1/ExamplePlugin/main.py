d3853c6d03eebe5ef4939159b72db78b9bc86398
skyss0fly <100278495+skyss0fly@users.noreply.github.com>
2023-07-05 12:02:00 +1000

    Update main.py

--- V0.1.0.1/ExamplePlugin/main.py
+++ V0.1.0.1/ExamplePlugin/main.py
@@ -1,5 +1,10 @@
 import PieRakNet as PieMC
 class main():
-
+ def __init__(self):
+   self.verion = 0.1
+   self.author = skyss0fly
+   self.API = 0.1.0.1
+   Self.Permissions = ExamplePlugin.perm
+   self.Commands = PieMC
   def onEnable():
     PieMC.logger.info("ExamplePlugin started:)")
