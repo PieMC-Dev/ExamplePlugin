c61cd472cf906f77f617f3624bbe17ce7535c8b8
skyss0fly <100278495+skyss0fly@users.noreply.github.com>
2023-07-31 17:09:29 +1000

    Update main.py

--- src/ExamplePlugin/main.py
+++ src/ExamplePlugin/main.py
@@ -1,5 +1,6 @@
-import PieRakNet as PieMC
-class main():
- def __init__(self):
-  def onEnable(self):
-    PieMC.logger.info("ExamplePlugin started:)")
+from piemc.plugin.PluginBase import PLuginBase
+from piemc.plugin.Terminal import Terminal
+
+class Main(PluginBase):
+  def on_enable(self):
+    self.logger.info(Terminal.green("[Example] enabling..."))
